bfc14778caeec6fa6db58148fbe3596f153647f8
Mohit07panwar <mohitpanwar2464@gmail.com>
2021-09-11 23:29:54 +0530

    Adding More Files and Changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Navbar from "./components/navbar.components.jsx";
 import TextForm from "./components/textform.jsx"
+// import DarkMode from './components/darkmode.jsx';
+
 
 function App() {
   return (
-   <div>
-     <Navbar title="TextUtils"/>
-   <div className="container my-4">
-    <TextForm text="Enter the Text"/>
-     </div> 
-</div>
-
-   
- 
+    <div>
+      <Navbar title="TextUtils" />
+      <div className="container my-4">
+        <TextForm text="Enter the Text" />
+        {/* <DarkMode /> */}
+      </div>
+    </div>
   )
 }
 
--- src/components/textform.jsx
+++ src/components/textform.jsx
@@ -4,18 +4,39 @@ import React, { useState } from "react";
 const TextForm = (props) => {
 
     const handleUpClick = () => {
-        // console.log(text);
         let newText = text.toUpperCase();
         setText(newText);
     }
     const handleLoClick = () => {
-        // console.log(text);
         let newText = text.toLowerCase();
         setText(newText);
     }
+    const handleClearClick = () => {
+        let newText = "";
+        setText(newText);
+    }
+    const handleCapatalizedClick = () => {
+        let arr = text.split(" ");
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+            //  console.log(arr);
+        }
+        //  console.log(arr);
+        let newText = arr.join(" "); // will join all the array elements in a string.
+        //  console.log(newText);
+        setText(newText);
+    }
+    const handleCopy = () => {
+        let text = document.getElementById("floatingTextarea").value;
+        navigator.clipboard.writeText(text);
+    }
+
+    const handleSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
 
     const handleOnChange = (e) => {
-        // console.log("OnChange");
         setText(e.target.value);
     }
 
@@ -30,6 +51,10 @@ const TextForm = (props) => {
             </div>
             <button type="button" className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To UpperCase</button>
             <button type="button" className="btn btn-secondary mx-2" onClick={handleLoClick}>Convert To LowerCase</button>
+            <button type="button" className="btn btn-warning mx-2" onClick={handleCapatalizedClick}>Capitalized Case</button>
+            <button type="button" className="btn btn-dark mx-2" onClick={handleSpaces}>Remove Extra Spaces</button>
+            <button type="button" className="btn btn-info mx-2" onClick={handleCopy}>Copy Text</button>
+            <button type="button" className="btn btn-success mx-2" onClick={handleClearClick}>Clear Text</button>
 
             <h1 className="my-3">Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} character</p>
